27f4049936a20c3417a620b4f34e0674a731be71
Oleh Topchak <Oleh_Topchak@epam.com>
2019-07-23 15:51:58 +0300

    Styled product list

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -6,7 +6,7 @@ import { Product } from '../../../core/interfaces';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
   @Input() product: Product;
